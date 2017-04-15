0b28ad034a71e019d981fbd9211f1089a6f31166
maxgurewitz <maxthegeek1@gmail.com>
2017-04-14 23:05:11 -0700

    use deployment options

--- src/main/java/com/LinkSaver/app/App.java
+++ src/main/java/com/LinkSaver/app/App.java
@@ -1,17 +1,30 @@
 package com.LinkSaver.app;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.json.JsonObject;
 
-public class App
-{
-	   public static void main(String[] args) {
-		    Vertx.vertx().deployVerticle(Server.class.getName(), e -> {	
-		    	if (e.failed()) {
-			    	System.out.println(e.cause());
-		    	} else {
-			    	System.out.println(e.result());
-		    	}
-		    });
-		    	
-	  }
+public class App {
+	private static final int DEFAULT_PORT = 8080;
+
+	public static void main(String[] args) {
+		JsonObject config = new JsonObject();
+
+		String portEnv = System.getenv("PORT");
+
+		int port = portEnv != null ? Integer.parseInt(portEnv) : DEFAULT_PORT;
+
+		config.put("http.port", port);
+
+		DeploymentOptions options = new DeploymentOptions().setConfig(config);
+
+		Vertx.vertx().deployVerticle(Server.class.getName(), options, e -> {
+			if (e.failed()) {
+				System.out.println(e.cause());
+			} else {
+				System.out.println("server started on port " + Integer.toString(port));
+				System.out.println(e.result());
+			}
+		});
+	}
 }
--- src/main/java/com/LinkSaver/app/Server.java
+++ src/main/java/com/LinkSaver/app/Server.java
@@ -5,24 +5,18 @@ import io.vertx.core.Future;
 
 public class Server extends AbstractVerticle {
 
-  @Override
-  public void start(Future<Void> fut) {
-    String portEnv = System.getenv("PORT");
-    Integer port = portEnv == null ? 8080 : Integer.parseInt(portEnv);
-    
-    vertx
-        .createHttpServer()
-        .requestHandler(r -> {
-          r.response().end("<h1>Hello from my first " +
-              "Vert.x 3 application</h1>");
-        })
-        .listen(port, result -> {
-          if (result.succeeded()) {
-            System.out.println("server started on port " + port);
-            fut.complete();
-          } else {
-            fut.fail(result.cause());
-          }
-        });
-  }
+	@Override
+	public void start(Future<Void> fut) {
+		int port = config().getInteger("http.port");
+
+		vertx.createHttpServer().requestHandler(r -> {
+			r.response().end("<h1>Hello from my first " + "Vert.x 3 application</h1>");
+		}).listen(port, result -> {
+			if (result.succeeded()) {
+				fut.complete();
+			} else {
+				fut.fail(result.cause());
+			}
+		});
+	}
 }
