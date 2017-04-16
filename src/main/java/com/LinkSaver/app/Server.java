23b88c5ec16fe02c2e6a307b2080d35eb5fe1e5d
maxgurewitz <maxthegeek1@gmail.com>
2017-04-15 17:02:42 -0700

    integrate sockjs

--- src/main/java/com/LinkSaver/app/Server.java
+++ src/main/java/com/LinkSaver/app/Server.java
@@ -3,6 +3,13 @@ package com.LinkSaver.app;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.ErrorHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeEventType;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
 public class Server extends AbstractVerticle {
 
@@ -12,12 +19,33 @@ public class Server extends AbstractVerticle {
 
 		HttpServer httpServer = vertx.createHttpServer();
 
+		Router router = Router.router(vertx);
+
+		router.route("/eventbus/*").handler(eventBusHandler());
+		router.route().failureHandler(ErrorHandler.create());
+		router.route().handler(StaticHandler.create().setCachingEnabled(false));
+
+		httpServer.requestHandler(router::accept);
+
 		httpServer.listen(port, result -> {
 			if (result.succeeded()) {
 				fut.complete();
 			} else {
 				fut.fail(result.cause());
 			}
+		});
+	}
+
+	private SockJSHandler eventBusHandler() {
+		PermittedOptions permittedOptions = new PermittedOptions().setAddressRegex("login");
+
+		BridgeOptions options = new BridgeOptions().addOutboundPermitted(permittedOptions);
+
+		return SockJSHandler.create(vertx).bridge(options, event -> {
+			if (event.type() == BridgeEventType.SOCKET_CREATED) {
+				System.out.println("A socket was created");
+			}
+			event.complete(true);
 		});
 	}
 }
