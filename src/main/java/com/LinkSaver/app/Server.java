1a183655f3cb7f2894303ce77ab1d940a588d6f8
maxgurewitz <maxthegeek1@gmail.com>
2017-05-02 00:57:05 -0700

    fix eventbus integration

--- src/main/java/com/LinkSaver/app/Server.java
+++ src/main/java/com/LinkSaver/app/Server.java
@@ -15,7 +15,7 @@ import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 import io.vertx.core.eventbus.EventBus;
 
 public class Server extends AbstractVerticle {
-	private static final String[] PERMITTED_ADDRESSES = { "create-user", "test" };
+	private static final String[] PERMITTED_ADDRESSES = { "create-user" };
 
 	@Override
 	public void start(Future<Void> fut) {
@@ -29,26 +29,6 @@ public class Server extends AbstractVerticle {
 
 		router.route("/static/*").handler(StaticHandler.create().setCachingEnabled(false));
 
-		router.route("/*").handler(routingContext -> {
-		  HttpServerResponse response = routingContext.response();
-		  response.putHeader("content-type", "text/plain");
-		  response.end("Hello World from Vert.x-Web!");
-		});
-
-		router.route().failureHandler(failureRoutingContext -> {
-		  int statusCode = failureRoutingContext.statusCode();
-		  
-		  if (statusCode >= 400) {
-    		  HttpServerResponse response = failureRoutingContext.response();
-    		  System.out.println("loc2" + response.getStatusMessage());
-    		  System.out.println("loc3" + failureRoutingContext);
-    		  System.out.println("loc4" + statusCode);
-    		  response.setStatusCode(statusCode).end("Oops, something went wrong");
-		  } else {
-			  failureRoutingContext.next();
-		  }
-  	    });
-
 		router.route().failureHandler(ErrorHandler.create());
 
 		httpServer.requestHandler(router::accept);
@@ -59,15 +39,7 @@ public class Server extends AbstractVerticle {
 				
 				eb.consumer("create-user", message -> {
 					System.out.println("create-user: " + message.body());			
-				});
-
-				eb.consumer("test").handler(message -> {
-//				eb.consumer("test", message -> {
-					System.out.println("test: " + message.body());			
-				});
-				
-				eb.consumer("create-user-result", message -> {
-					System.out.println("create-user-result: " + message.body());			
+					message.reply("{ \"name\": \"max\" }");
 				});
 
 				fut.complete();
@@ -80,10 +52,7 @@ public class Server extends AbstractVerticle {
 	private SockJSHandler eventBusHandler() {
 		PermittedOptions permittedOptions = new PermittedOptions();
 
-//		permittedOptions.setAddressRegex(".*");
-
 		for (String address : PERMITTED_ADDRESSES) {
-			System.out.println("loc1" + address);
 			permittedOptions.setAddress(address);
 		}
 
